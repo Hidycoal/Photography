9d4d1a11359d449d5d72c9df4d72b7f60f71a6f0
FIFELOLA IDOWU <hidycoal@gmail.com>
2025-03-07 18:56:18 -0600

    latest commit

--- src/components/AnimRoutes.js
+++ src/components/AnimRoutes.js
@@ -4,16 +4,21 @@ import About from '../pages/About';
 import Contact from '../pages/Contact';
 import Portfolio from '../pages/Portfolio';
 
-import { Routes, Route, useLocation } from 'react-router-dom'
+import { Routes, Route, useLocation } from 'react-router-dom';
+import { AnimatePresence } from 'framer-motion';
 
 const AnimRoutes = () => {
+  const location = useLocation();
   return (
-    <Routes>
-      <Route path="/" element={<Home />}/>
-      <Route path="/about" element={<About />}/>
-      <Route path="/contact" element={<Contact />}/>
-      <Route path="/portfolio" element={<Portfolio />}/>
-    </Routes>
+    <AnimatePresence initial={true} mode='wait'>
+    
+      <Routes key={location.pathname} location={location} >
+        <Route path="/" element={<Home />}/>
+        <Route path="/about" element={<About />}/>
+       <Route path="/contact" element={<Contact />}/>
+        <Route path="/portfolio" element={<Portfolio />}/>
+      </Routes>
+    </AnimatePresence>
 
   );
 };
--- src/components/Socials.js
+++ src/components/Socials.js
@@ -1,7 +1,31 @@
 import React from 'react';
+import {ImFacebook, ImTwitter, ImInstagram, ImGithub} from 'react-icons/im'
 
 const Socials = () => {
-  return <div>Socials</div>;
+  return <div className='hidden xl:flex ml-24'>
+    <ul className='flex gap-x-4'>
+      <li>
+        <a href="https://www.facebook.com/hidycoal" target='_blank'>
+          <ImFacebook />
+        </a>
+      </li>
+      <li>
+        <a href="https://www.twitter.com/hidycoal" target='_blank'>
+          <ImTwitter />
+        </a>
+      </li>
+      <li>
+        <a href="https://www.instagram.com/hidycoal" target='_blank'>
+          <ImInstagram />
+        </a>
+      </li>
+      <li>
+        <a href="https://www.github.com/hidycoal" target='_blank'>
+          <ImGithub />
+        </a>
+      </li>
+    </ul>
+  </div>;
 };
 
 export default Socials;
